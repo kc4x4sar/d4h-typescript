cd0c024f7e69d593c50b8cb9553a7618c5fc3fc6
Colin Williams <colin.williams@kc4x4sar.org>
2023-03-19 18:49:38 -0700

    Fixed top-level exports to include customField

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import D4H from './src/d4h.js'
 export default D4H
 
+export * from './src/customField'
 export * from './src/d4h.js'
 export * from './src/group.js'
 export * from './src/member.js'
